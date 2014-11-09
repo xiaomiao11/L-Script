193fdf44ce9088c2279ab49bc6ad123ee559c39c
ergou <654955321@qq.com>
2014-11-09 14:15:30 +0800

    2014.11.9

--- FuckingAwesomeLeeSin/Properties/AssemblyInfo.cs
+++ FuckingAwesomeLeeSin/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("FuckingAwesomeLeeSin")]
+[assembly: AssemblyTitle("3189263盲僧")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Microsoft")]
-[assembly: AssemblyProduct("FuckingAwesomeLeeSin")]
+[assembly: AssemblyCompany("苦涩")]
+[assembly: AssemblyProduct("3189263盲僧")]
 [assembly: AssemblyCopyright("Copyright © Microsoft 2014")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
